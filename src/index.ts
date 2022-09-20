c2b4b695f5800a4ae80863777611d0b414664850
WaScript3r <wascript3r@gmail.com>
2022-09-20 21:18:51 +0300

    added port config

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ const io = new SocketServer(httpServer, {
 
 new GameServer(io)
 
-httpServer.listen(process.env.PORT || 3000, () => {
-	console.log('Listening on port 3000')
+const port = process.env.PORT || 3000
+httpServer.listen(port, () => {
+	console.log('Listening on port ' + port)
 })
