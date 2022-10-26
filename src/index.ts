4d350feb4cf13c7029042c853df4ec7d20aff1b8
WaScript3r <wascript3r@gmail.com>
2022-10-26 15:26:37 +0300

    added dependencies to game ucase

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,58 @@
-import { createLevel, FirstLevel } from "./level/Level";
-import express from "express";
-import * as http from "http";
-import { Server as SocketServer } from "socket.io";
+import express from 'express'
+import * as http from 'http'
+import { Server as SocketServer } from 'socket.io'
 
-import * as session from "./session";
-import * as lobby from "./lobby";
-import * as game from "./game";
-import * as player from "./player";
-import * as server from "./server";
-import { IObserver } from "./util/Observer";
+import * as session from './session'
+import * as lobby from './lobby'
+import * as game from './game'
+import * as player from './player'
+import * as server from './server'
+import { IObserver } from './util/Observer'
 
-const app = express();
-const httpServer = http.createServer(app);
+const app = express()
+const httpServer = http.createServer(app)
 const io = new SocketServer(httpServer, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
+	cors: {
+		origin: '*',
+		methods: ['GET', 'POST']
+	}
+})
 
-const sessions = new session.Sessions();
-const lobbies = new lobby.Lobbies();
+const sessions = new session.Sessions()
+const lobbies = new lobby.Lobbies()
 
-const playerPub = new player.Publisher();
-const lobbyPub = new lobby.Publisher();
-const gamePub = new game.Publisher();
+const playerPub = new player.Publisher()
+const lobbyPub = new lobby.Publisher()
+const gamePub = new game.Publisher()
 
-const sessionUcase = new session.Usecase(sessions);
-const playerUcase = new player.Usecase(sessions, playerPub);
-const lobbyUcase = new lobby.Usecase(lobbies, playerUcase, lobbyPub);
-const gameUcase = new game.Usecase(lobbies, gamePub);
+const lobbyFacade = new lobby.Facade(lobbies)
+
+const sessionUcase = new session.Usecase(sessions)
+const playerUcase = new player.Usecase(sessions, playerPub)
+const lobbyUcase = new lobby.Usecase(lobbies, playerUcase, lobbyPub)
+const gameUcase = new game.Usecase(lobbies, gamePub, lobbyFacade)
 
 const obs: Array<IObserver> = [
-  new lobby.PlayerObserver(playerPub, io, lobbyUcase),
-  new lobby.Observer(lobbyPub, io, lobbyUcase),
+	new lobby.PlayerObserver(playerPub, io, lobbyUcase),
+	new lobby.Observer(lobbyPub, io, lobbyUcase),
 
-  new game.LobbyObserver(lobbyPub, io, gameUcase),
-  new game.PlayerObserver(playerPub, gameUcase),
-  new game.Observer(gamePub, io, gameUcase),
-];
-obs.forEach((o) => o.start());
+	new game.LobbyObserver(lobbyPub, io, gameUcase),
+	new game.PlayerObserver(playerPub, gameUcase),
+	new game.Observer(gamePub, io, gameUcase)
+]
+obs.forEach((o) => o.start())
 
-const playerHndFact = new player.HandlerFactory(io, playerUcase, sessionUcase);
-const lobbyHndFact = new lobby.HandlerFactory(lobbyUcase, sessionUcase);
-const gameHndFact = new game.HandlerFactory(gameUcase, sessionUcase);
+const playerHndFact = new player.HandlerFactory(io, playerUcase, sessionUcase)
+const lobbyHndFact = new lobby.HandlerFactory(lobbyUcase, sessionUcase)
+const gameHndFact = new game.HandlerFactory(gameUcase, sessionUcase)
 
 new server.Server(io)
-  .addHandlerFactory(lobbyHndFact)
-  .addHandlerFactory(playerHndFact)
-  .addHandlerFactory(gameHndFact)
-  .registerListeners();
+	.addHandlerFactory(lobbyHndFact)
+	.addHandlerFactory(playerHndFact)
+	.addHandlerFactory(gameHndFact)
+	.registerListeners()
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 httpServer.listen(port, () => {
-  console.log("Listening on port " + port);
-});
+	console.log('Listening on port ' + port)
+})
